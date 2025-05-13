8d23f9e2709511fe326a35048c44a14d0a79bc2d
afifihsnm <afifihsanke2@gmail.com>
2025-05-13 18:15:37 +0700

    feat: add keywords for better seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Afif Ihsan | Full Stack Developer & DevOps Engineer Freelancer",
   description: "I help businesses build scalable web apps and cloud infrastructure. Full Stack + DevOps freelancer for hire. Based remotely, serving globally.",
+  keywords: [
+    "Afif Ihsan Maulana",
+    "Web Development Services",
+    "Freelance web developer for small businesses",
+    "Afif Ihsan Maulana fullstack developer",
+    "AWS engineer",
+    "Freelancer portfolio website",
+  ],
 };
 
 export default function RootLayout({
